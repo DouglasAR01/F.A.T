9b18483c31155163ea6dcbc9888d87dbf12a0528
Doukuest <33143337+Doukuest@users.noreply.github.com>
2019-02-10 11:02:38 -0500

    SetUp del proyecto de BlueJ

---
+++ Banner.java
@@ -0,0 +1,26 @@
+/**
+ * La clase Banner es de donde se heredan los métodos y atributos
+ * necesarios para el correcto funcionamiento de BannerAprendiz y BannerTutor
+ * @author: Douglas R
+ * Date: 10/02/2019
+ */
+public abstract class Banner
+{
+    private String materia;
+    private String rama;
+    private String tema;
+    private String fecha;
+    private String lugar;
+    
+    /**
+     * Este método permite eliminar el banner cuando el usuario lo solicite
+     */
+    public void eliminarBanner(){
+        
+    }
+    
+    public void contactarAutor(){
+        
+    }
+   
+}
